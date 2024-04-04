7a5a038e5c64ed8c054524743efbc77ea88da116
tomo wakamatsu <165577213+tomoWakamatsu@users.noreply.github.com>
2024-04-04 21:59:46 +0900

    Add files via upload

--- FrostyFriday-Week1.sql
+++ FrostyFriday-Week1.sql
@@ -1,17 +1,28 @@
 /*  https://frostyfriday.org/blog/2022/07/14/week-1/
 
-    FrostyFriday Inc., your benevolent employer, has an S3 bucket that is filled with .csv data dumps. 
-    This data is needed for analysis. Your task is to create an external stage, and load the csv files 
-    directly from that stage into a table.
+　　　　　　　シナリオ：
+
+　　　　　　　　あなたの雇用主であるFrostyFriday Inc.は、.csvデータダンプで満たされたS3バケットを持っている。
+    このデータは分析に必要です。あなたのタスクは、外部ステージを作成し、そのステージから直接csvファイルをテーブルにロードすることです。
+    ファイルを直接テーブルにロードしてください
     
     The S3 bucket’s URI is: s3://frostyfridaychallenges/challenge_1/
 */
+
+
+use role sysadmin;
+use warehouse compute_wh;
+
+-- ***solution 01 まずはベーシックな回答*---*---*---*---*---*---*---*---*---*---**---*---*---*---*---*
+-- commited by darylkit
+-- https://github.com/darylkit/Frosty_Friday/blob/main/Week%201%20-%20External%20Stages/external_stages.sql
+
 
 --まずはDBやスキーマを作るよ
 --ここはなんでもOK
 create database if not exists frosty_friday;
 create schema if not exists frosty;
-drop schema if exists public;
+USE SCHEMA frosty_friday.frosty ;
 
 --S3の外部ステージを作るよ
 --GUIから作ってもOK
@@ -23,9 +34,6 @@ create or replace stage data_dumps
 list @data_dumps;
 
 
--- ***solution 01 まずはベーシックな回答
--- commited by darylkit
--- https://github.com/darylkit/Frosty_Friday/blob/main/Week%201%20-%20External%20Stages/external_stages.sql
 --テーブル作成
 create table if not exists data_dump as 
 select $1 as val from @data_dumps;
@@ -33,20 +41,23 @@ select $1 as val from @data_dumps;
 -- どんなデータができたかみてみよう
 select * from data_dump;
 
+--- お掃除
+drop database frosty_friday;
+
 ---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*
 
 
--- ***solution 0２　実務であるあるパターン
+-- ***solution 0２　実務であるあるパターン*---*---*---*---*---*---*---*---*---*---**---*---*---*---*---*
 -- commited by tshoji
 -- https://github.com/taksho/Frosty_Friday/blob/main/Week1/external_stages.sql
 
 -- Set the SYSADMIN role
 use role sysadmin;
 
--- Create warehouse, Database, schema
-create or replace warehouse ff_wh AUTO_SUSPEND=1;
+-- Database, schema
 create or replace database ff_db;
 create or replace schema ff_db.ff_schema;
+USE SCHEMA ff_db.ff_schema ;
 
 -- Create S3 External Stage
 create or replace stage ff_db.ff_schema.ff_s3
@@ -93,17 +104,35 @@ COPY into week1csv from (
     file_format = (format_name = 'csv_ff')
 ;
 
+-- Select the result by the right order
+select * from week1csv order by filename, file_row_number;
+
 -- Delete the NULL rows
 delete from week1csv where result is null;
 
 -- Select the result by the right order
 select * from week1csv order by filename, file_row_number;
 
+
+--- お掃除
+drop database ff_db;
+
 ---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*
 
--- ***solution 03　複数行の文字列データを指定したカラムの値ごとに1行に集約するパターン
+-- ***solution 03　複数行の文字列データを指定したカラムの値ごとに1行に集約するパターン*---*---*---*---*---*---*---*
 -- https://github.com/JavaCaste/snow/blob/main/Week1_Basic_ExternalStages.sql
 -- file format
+
+CREATE DATABASE SAMPLES;
+CREATE SCHEMA SAMPLES.FROSTY;
+USE SCHEMA SAMPLES.FROSTY ;
+
+-- create stage
+CREATE STAGE Week1_Basic_ExternalStage url='s3://frostyfridaychallenges/challenge_1/';
+
+list @Week1_Basic_ExternalStage;
+
+
 CREATE OR REPLACE FILE FORMAT csv_ff
     TYPE = 'csv' 
     SKIP_HEADER=1
@@ -114,19 +143,20 @@ CREATE OR REPLACE FILE FORMAT csv_ff
 -- LISTAGG https://docs.snowflake.com/ja/sql-reference/functions/listagg
 -- LISTAGGとは・・・複数行の文字列データを指定したカラムの値ごとに1行に集約できる
 -- https://dev.classmethod.jp/articles/snowflake-listagg/
-SELECT LISTAGG($1,' ') WITHIN GROUP 
-(ORDER BY METADATA$FILENAME, METADATA$FILE_ROW_NUMBER) AS COL1  --WITHIN GROUPでリスト内の各グループの値の順序を決定します。ファイル名とファイルNoの順番出ないと変な言葉になるよ
-FROM @data_dumps (file_format=>'csv_ff');
+SELECT LISTAGG($1,' ') WITHIN GROUP (ORDER BY METADATA$FILENAME, METADATA$FILE_ROW_NUMBER) AS COL1
+FROM @Week1_Basic_ExternalStage (file_format=>'csv_ff');
 
 
 -- テストテーブルだし、TEMPテーブルで作るのも良いよね
 CREATE TEMPORARY TABLE FROSTY.Week1_Basic_ExternalStage AS 
 (SELECT LISTAGG($1,' ') WITHIN GROUP (ORDER BY METADATA$FILENAME, METADATA$FILE_ROW_NUMBER) AS COL1
-FROM @data_dumps  (file_format=>'csv_ff') );
-
-select * from FROSTY.Week1_Basic_ExternalStage;
+FROM @FROSTY.Week1_Basic_ExternalStage  (file_format=>'csv_ff') );
 
 
---- お掃除
-drop table data_dump;
-drop table Week1_Basic_ExternalStage;
+SELECT * FROM FROSTY.Week1_Basic_ExternalStage;
+
+-- お掃除
+drop database SAMPLES;
+
+*---*---*---*---*---*---*---*---*---*---**---*---*---*---*---*---*---*---*---*---**---*---*---*---*
+
